90419d1052e4785a83cee9111623ed8590bcb06f
L-puzzello <lpuzzell@gmail.com>
2022-04-07 11:52:25 +1000

    Add function documentation

--- calc.py
+++ calc.py
@@ -1,17 +1,6 @@
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
-    else:
-        print('unknown operator!')
-        return num0 + num1
-    else:
-        print('unknown operator!')
-        return None
-    def compute(expression):
     values = expression.split(' ')
     num0 = int(values[0])
     operator = values[1]
