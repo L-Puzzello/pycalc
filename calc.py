012eeeabd3ce57f957dc203849801bc0204e77d0
L-puzzello <lpuzzell@gmail.com>
2022-04-07 11:23:15 +1000

    Return none, not 0, on invalid input

--- calc.py
+++ calc.py
@@ -7,4 +7,7 @@ def compute(expression):
         return num0 + num1
     else:
         print('unknown operator!')
-        return 0
+        return num0 + num1
+    else:
+        print('unknown operator!')
+        return None
